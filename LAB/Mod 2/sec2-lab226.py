a3109066f01acbe770c58fe6b7c8323fdafb8e38
wølf <55291037+St4rkW0lf@users.noreply.github.com>
2025-10-23 12:04:00 -0300

    Create sec2-lab226.py

---
+++ LAB/Mod 2/sec2-lab226.py
@@ -0,0 +1,5 @@
+# Cenário
+# Escreva um código de uma linha usando a função de print(), bem como os caracteres de nova linha e de escape, para corresponder ao resultado esperado gerado em três linhas.
+
+print('"Eu sou"\n""aprendizado""\n"""Python"""')
+
