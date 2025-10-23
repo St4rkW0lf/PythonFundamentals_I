0983742e7b3b02de2d5cc6ad1ad5fb6b09698a23
wølf <55291037+St4rkW0lf@users.noreply.github.com>
2025-10-23 11:59:25 -0300

    Create sec1-lab215.py 🐍

---
+++ LAB/Mod 2/sec1-lab215.py
@@ -0,0 +1,12 @@
+# Cenário
+# O comando print(), que é uma das diretrizes mais fáceis em Python, simplesmente imprime uma linha na tela.
+#
+# Em seu primeiro laboratório:
+#
+# Use a função de print() para imprimir a linha Olá, Python! na tela. Use aspas duplas ao redor da string.
+# Depois de fazer isso, use a função print() novamente, mas desta vez imprima seu primeiro nome.
+# Remova as aspas duplas e execute o código. Assista à reação do Python. Que tipo de erro é gerado?
+# Em seguida, remova os parênteses, coloque aspas duplas e execute o código novamente. Que tipo de erro é gerado desta vez?
+# Experimente o máximo que puder. Altere aspas duplas para aspas simples, use várias funções print() na mesma linha e, em seguida, em linhas diferentes. Veja o que acontece.
+
+print("Olá, Python!")
