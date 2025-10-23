15efc8598a8bfe1e0b772afa15a4e5ade6081605
wølf <55291037+St4rkW0lf@users.noreply.github.com>
2025-10-23 12:10:00 -0300

    Create sec6-lab269.py

---
+++ LAB/Mod 2/sec6-lab269.py
@@ -0,0 +1,19 @@
+# Cenário
+# Sua tarefa é completar o código para avaliar os resultados de quatro operações aritméticas básicas.
+# 
+# Os resultados devem ser impressos no console.
+# 
+# Talvez você não consiga proteger o código de um usuário que deseja dividir por zero. Tudo bem, não se preocupe com isso por enquanto.
+# 
+# Teste seu código ‒ ele produz os resultados esperados?
+# 
+# Não mostraremos nenhum dado de teste - isso seria muito simples.
+
+a = float(input("Entre com o primeiro valor: "))
+b = float(input("Entre com o segundo valor: "))
+print("Adição:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+print("\nIsso é tudo, pessoal!")
+
